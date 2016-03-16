37d124e8bdf745f96faa6dd60cdf2f00f83dda12
kdonev <kd@kd>
2016-03-16 22:48:37 +0200

    First commit

---
+++ Assets/Bagpipe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class Bagpipe
+	{
+		public static readonly string[] VisibleNotes = {
+			"_G", "_A", "_B", "C", "D", "E", "F", "#F", "G", "A" };
+
+		public static readonly string[] RealNotes = {
+			"_A", "_B", "_#C", "D", "E", "#F", "G", "#G", "A", "B" };
+
+		public static readonly bool[][] FingerPositions = {
+		//    R F1   R F2   R F3   R F4   R F5   L F1   L F2   L F3   L F4   L F5
+			{ false, false, false, false, false, false, false, false, false, false },   // _G
+			{ false, false, false, false, false, false, false, false, false, true  },   // _A
+			{ false, false, false, false, false, false, false, false, true , false },   // _B
+			{ false, false, false, false, false, false, false, true , false, false },   // C
+			{ false, false, false, false, false, false, true , false, false, false },   // D
+			{ false, false, false, true , false, false, false, false, false, false },   // E
+			{ false, true , false, true , false, false, false, false, false, false },   // F
+			{ false, false, true , false, false, false, false, false, false, false },   // #F
+			{ false, true , true , false, false, false, false, false, false, false },   // G
+			{ true , false, false, false, false, false, false, false, false, false },   // A
+		};
+
+		public Bagpipe ()
+		{
+		}
+	}
+}
+
---
+++ Assets/Hand/HandController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandController : MonoBehaviour {
+
+	public bool f1Up;
+	public bool f2Up;
+	public bool f3Up;
+	public bool f4Up;
+	public bool f5Up;
+
+	private bool f1WasUp;
+	private bool f2WasUp;
+	private bool f3WasUp;
+	private bool f4WasUp;
+	private bool f5WasUp;
+
+	Animator _f1Animator;
+	Animator _f2Animator;
+	Animator _f3Animator;
+	Animator _f4Animator;
+	Animator _f5Animator;
+
+	// Use this for initialization
+	void Start () {
+		_f1Animator = transform.FindChild ("F1").gameObject.GetComponent<Animator> ();
+		_f2Animator = transform.FindChild ("F2").gameObject.GetComponent<Animator> ();
+		_f3Animator = transform.FindChild ("F3").gameObject.GetComponent<Animator> ();
+		_f4Animator = transform.FindChild ("F4").gameObject.GetComponent<Animator> ();
+		_f5Animator = transform.FindChild ("F5").gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleFinger (_f1Animator, f1Up, ref f1WasUp);
+		HandleFinger (_f2Animator, f2Up, ref f2WasUp);
+		HandleFinger (_f3Animator, f3Up, ref f3WasUp);
+		HandleFinger (_f4Animator, f4Up, ref f4WasUp);
+		HandleFinger (_f5Animator, f5Up, ref f5WasUp);
+	}
+
+	void HandleFinger(Animator f, bool newVal, ref bool oldVal)
+	{
+		if (oldVal != newVal) 
+		{
+			oldVal = newVal;
+			if (newVal)
+				f.SetTrigger ("Up");
+			else
+				f.SetTrigger ("Down");
+		}
+	}
+}
